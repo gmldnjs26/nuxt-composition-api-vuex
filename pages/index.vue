b26b7df60134ca80606cc58c78ec6cec50a3a7c1
gmldnjs26 <imleesky265@gmail.com>
2022-06-18 16:28:50 +0900

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
-    {{ hogeName }} / {{ hogeCounter }}
     <div>
-      <button @click="changeName">Name</button>
-      <button @click="changeCount">Count</button>
-      <button @click="changeHoge">All</button>
+      <span>hogeName: </span>
+      {{ hogeName }}
+      <button @click="changeHogeName">ChangHogeName</button>
+    </div>
+    <div>
+      <span>hogeCount: </span>
+      {{ hogeCount }}
+      <button @click="changeHogeCount">ChangHogeCount</button>
+    </div>
+    <div>
+      <span>nestedHogeName: </span>
+      {{ hogeNestedName }}
+      <button @click="changeNestedHogeName">ChangNestedHogeName</button>
+    </div>
+    <div>
+      <span>nestedHogeCount: </span>
+      {{ hogeNestedCount }}
+      <button @click="changeNestedHogeCount">ChangNestedHogeCount</button>
     </div>
   </div>
 </template>
@@ -17,26 +31,46 @@ export default defineComponent({
   setup() {
     const store = useStore()
     const hogeName = computed<string>(() => {
+      return store.getters['hoge/getHogeName']
+    })
+    const hogeCount = computed<number>(() => {
+      return store.getters['hoge/getHogeCounter']
+    })
+    const hogeNestedName = computed<string>(() => {
       return store.getters['hoge/getNestedHogeName']
     })
-    const hogeCounter = computed<number>(() => {
+    const hogeNestedCount = computed<number>(() => {
       return store.getters['hoge/getNestedHogeCounter']
     })
-    const changeCount = () => {
-      store.dispatch('hoge/incrementNestedHogeCount', hogeCounter.value + 1)
+    const changeHogeCount = () => {
+      store.dispatch('hoge/incrementHogeCount', hogeCount.value + 1)
     }
-    const changeName = () => {
-      store.dispatch('hoge/changeNestedHogeName', 'sampleName')
+    const changeHogeName = () => {
+      store.dispatch('hoge/changeHogeName', hogeName.value + '_1')
+    }
+    const changeNestedHogeCount = () => {
+      store.dispatch('hoge/incrementNestedHogeCount', hogeNestedCount.value + 1)
+    }
+    const changeNestedHogeName = () => {
+      store.dispatch('hoge/changeNestedHogeName', hogeNestedName.value + '_2')
     }
     const changeHoge = () => {
-      store.dispatch('hoge/chageHoge', { hogeCount: 999, hogeName: 'reative' })
+      store.dispatch('hoge/chageHoge', {
+        hogeCount: 999,
+        hogeName: 'reative',
+        nestedHoge: { hogeCount: 999, hogeName: 'nestedReative' },
+      })
     }
 
     return {
       hogeName,
-      hogeCounter,
-      changeCount,
-      changeName,
+      hogeCount,
+      hogeNestedName,
+      hogeNestedCount,
+      changeHogeCount,
+      changeHogeName,
+      changeNestedHogeCount,
+      changeNestedHogeName,
       changeHoge,
     }
   },
